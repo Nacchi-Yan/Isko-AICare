2d01e0dd06301c94f0c4bfc2b71ed88138c552be
Nacchi-Yan <83501590+Nacchi-Yan@users.noreply.github.com>
2025-03-19 20:38:02 +0800

    db sqllite

--- iskoaicare/settings.py
+++ iskoaicare/settings.py
@@ -78,13 +78,10 @@ WSGI_APPLICATION = 'iskoaicare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': ,
-        'USER': ,
-        'PASSWORD': ,
-        'HOST': ,
-        'PORT': ,}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
